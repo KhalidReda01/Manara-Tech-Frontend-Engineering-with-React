85ec7a4da4e04aadaaa62ba8018c5ed663995f97
khalid Reda <khalidaziz2198@gmail.com>
2024-10-25 17:41:03 +0300

    A bug

--- Course 6 Asynchronous JavaScript/2.Callbacks, Promises, and AsyncAwait/app.js
+++ Course 6 Asynchronous JavaScript/2.Callbacks, Promises, and AsyncAwait/app.js
@@ -9,16 +9,52 @@
 
 // console.log("test");
 // nested setTimeout outs
-setTimeout(() => {
-  console.log("3");
-  setTimeout(() => {
-    console.log("2");
-    setTimeout(() => {
-      console.log("1");
-    }, 1000);
-  }, 1000);
-}, 1000);
+// setTimeout(() => {
+//   console.log("3");
+//   setTimeout(() => {
+//     console.log("2");
+//     setTimeout(() => {
+//       console.log("1");
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
 // button in event handler in browser Javascript
 // let btn;
 // btn.addEventListner("click", () => {});
 // this code run in node we don't have access to the dom
+// error first callback
+// fs.readFile("./test.text", { encoding: "utfa-8" }, (err, data) => {
+//   if (err) {
+//     console.log("Error");
+//     console.log(err);
+//   } else {
+//     console.log("Got Data");
+//     console.log(data);
+//   }
+// });
+
+// Create a promise
+// const myPromise = new Promise((resolve, reject) => {
+//   const rand = Math.floor(Math.random() * 6) + 1;
+//   if (rand === 0) {
+//     resolve();
+//   } else {
+//     reject();
+//   }
+// });
+// myPromise
+//   .then(() => console.log("Success"))
+//   .catch(() => console.error("something is wrong"));
+
+// fetch with promises
+fetch(" https://pokeapi.co/api/v2/pokemon/ditto")
+  .then((res) => res.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+//fetch Pokemon with async / await without error handling
+const fetchPokemon = async (id) => {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id})`);
+  const data = await res.json();
+  console.log(data);
+};
+fetchPokemon(2);
