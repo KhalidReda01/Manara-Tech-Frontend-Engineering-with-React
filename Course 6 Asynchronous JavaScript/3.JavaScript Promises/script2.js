75f01af964415c8fb2f4dd9a8d484ddf4398a9b3
khalid Reda <khalidaziz2198@gmail.com>
2024-10-27 11:40:56 +0300

    Review Promises start

--- Course 6 Asynchronous JavaScript/3.JavaScript Promises/script2.js
+++ Course 6 Asynchronous JavaScript/3.JavaScript Promises/script2.js
@@ -1,1 +1,21 @@
-console.log("test");
+// doJimWork = () => {
+//   return true;
+// };
+// function handleJimWork(successCallback, errorCallback) {
+//   // Slow method that runs in the background
+//   const success = doJimWork();
+//   if (success) {
+//     successCallback();
+//   } else {
+//     errorCallback();
+//   }
+// }
+
+// handleJimWork(
+//   () => {
+//     console.log("Success");
+//   },
+//   () => {
+//     console.error("Error");
+//   }
+// );
